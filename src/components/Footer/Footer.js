1ce9855734848cd07498f44637e88f507e53a553
Chris Yerkes <hello@mopsdigital.com>
2022-08-23 17:24:50 -0400

    Finishing simple react component layout. Starting to try to get acf fields pulled into app.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedinIn, faGithub, faCodepen, faTwitter } from '@fortawesome/free-brands-svg-icons';
 import Link from 'next/link';
 
 import useSite from 'hooks/use-site';
@@ -18,7 +20,7 @@ const Footer = () => {
   const hasMenu = hasRecentPosts || hasRecentCategories;
 
   return (
-    <footer className={styles.footer}>
+    /* <footer className={styles.footer}>
       {hasMenu && (
         <Section className={styles.footerMenu}>
           <Container>
@@ -90,6 +92,40 @@ const Footer = () => {
           </p>
         </Container>
       </Section>
+    </footer> */
+    <footer id="main-footer">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-sm-6 text-center text-sm-start mb-3 mb-sm-0">
+            <p className="name">Chris Yerkes</p>
+            <p className="copyright">&copy; {new Date().getFullYear()} Mops Digital, LLC. All rights reserved.</p>
+          </div>
+          <div className="col-sm-6 footer-social-nav">
+            <ul className="nav justify-content-center justify-content-sm-end social-nav white-social-nav">
+              <li className="nav-item linkedin">
+                <a href="#" className="nav-link">
+                  <FontAwesomeIcon icon={faLinkedinIn} />
+                </a>
+              </li>
+              <li className="nav-item github">
+                <a href="#" className="nav-link">
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+              <li className="nav-item codepen">
+                <a href="#" className="nav-link">
+                  <FontAwesomeIcon icon={faCodepen} />
+                </a>
+              </li>
+              <li className="nav-item twitter">
+                <a href="#" className="nav-link">
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default function Home({ posts, pagination }) {
             __html: description,
           }}
         />
-      </Header> */}
+      </Header> 
 
       <Section>
         <Container>
@@ -64,7 +64,7 @@ export default function Home({ posts, pagination }) {
             />
           )}
         </Container>
-      </Section>
+      </Section> */}
     </Layout>
   );
 }
